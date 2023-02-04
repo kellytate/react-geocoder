7c50672dadd14fe12ae005af68b226d29ea9d10f
kelly <kelly.tate.dev@gmail.com>
2023-02-04 08:01:46 -0800

    Updated App test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/List of Cats/i);
+  const linkElement = screen.getByText(/Latitude and Longitude/i);
   expect(linkElement).toBeInTheDocument();
 });
